192d4d6da6898767463bf183e4ec4beefae262ed
Calvin Foster <cal.j.foster@gmail.com>
2019-10-21 23:32:55 -0500

    new commit

--- Palindromes.Tests/UnitTest1.cs
+++ Palindromes.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Palindromes.Tests
         public void Test1()
         {
             //Arrange
-            var palindromes = "Do Geese See God";
+            var palindromes = "A Santa Lived As a Devil At NASA";
             var checkPalindrome = new CheckPalindrome();
 
 
