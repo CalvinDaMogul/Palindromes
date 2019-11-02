9d2fa66d38834a9af90718ee3e07ba928f9f406c
Calvin Foster <cal.j.foster@gmail.com>
2019-11-01 23:47:48 -0500

    another commit

--- Palindromes.Tests/UnitTest1.cs
+++ Palindromes.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Palindromes.Tests
         public void Test1()
         {
             //Arrange
-            var palindromes = "Kayak";
+            var palindromes = "Racecar";
             var checkPalindrome = new CheckPalindrome();
 
 
